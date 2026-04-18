5c16849d6555c85ad3228605cb69d76282a14749
Kamille <kamillehartmannm@gmail.com>
2026-04-17 21:11:36 -0300

    validação adicional de floats

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ app = FastAPI()
 
 @app.get("/sum")
 def sum_numbers(a: float, b: float):
+	try:
+		a = float(a)
+		b = float(b)  
+	except Exception as e:
+		return JSONResponse(content={"error": "Invalid input. Please provide numeric values for a and b."}, status_code=400)
+	
 	result = a + b
 	return {"result": result}
 
