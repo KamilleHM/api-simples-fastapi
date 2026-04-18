b6e7925eeebad2f677fc37520249d9aa80f402a9
Kamille <kamillehartmannm@gmail.com>
2026-04-17 21:12:45 -0300

    subtração agora

--- main.py
+++ main.py
@@ -16,6 +16,18 @@ def sum_numbers(a: float, b: float):
 	result = a + b
 	return {"result": result}
 
+
+@app.get("/subtract")
+def subtract_numbers(a: float, b: float):
+	try:
+		a = float(a)
+		b = float(b)
+	except Exception as e:
+		return JSONResponse(content={"error": "Invalid input. Please provide numeric values for a and b."}, status_code=400)
+
+	result = a - b
+	return {"result": result}
+
 # To run: uvicorn main:app --reload
 
 if __name__ == "__main__":
